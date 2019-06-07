44b1a5b0457bcfb3daf35e30df4bf20e14a59c8c
MariusA <anca.ticau@gmail.com>
2019-06-07 19:54:54 +0300

    Exercise 13

---
+++ Strings-Ex13/Strings-Ex13/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Strings_Ex13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string givenString = "This is a string";
+            Console.WriteLine(AddSufix(givenString));
+        }
+
+        //Write a method to add 'ing' at the end of a given string (length should be at least 3). If the given string already ends with 'ing' then add 'ly' instead.
+        //If the string length of the given string is less than 3, leave it unchanged.
+        public static string AddSufix(string input)
+        {
+            if (input.Length < 3)
+            {
+                return input;
+            }
+
+            string sufix1 = "ing";
+            string sufix2 = "ly";
+
+            if (input.LastIndexOf(sufix1, StringComparison.InvariantCulture) == -1)
+            {
+                input += sufix1;
+            }
+            else
+            {
+                input += sufix2;
+            }
+
+            return input;
+        }
+    }
+}
